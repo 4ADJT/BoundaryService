882d874112123e1ad3909c4d2f88bed2f0d065e9
brocchirodrigo <rodrigo.brocchi@gmail.com>
2024-07-28 09:39:06 -0300

    config: Swagger url.

--- src/main/java/br/com/fiap/boundaryservice/BoundaryServiceApplication.java
+++ src/main/java/br/com/fiap/boundaryservice/BoundaryServiceApplication.java
@@ -1,9 +1,14 @@
 package br.com.fiap.boundaryservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 
 @SpringBootApplication
 @EnableMongoAuditing
